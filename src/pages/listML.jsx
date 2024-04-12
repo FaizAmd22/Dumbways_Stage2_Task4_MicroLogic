4c2884f4c1311d5751162a3e120978a1b3e7f350
Faizhal Ahmad <ahmadfaizhal8@gmail.com>
2024-04-12 17:33:08 +0700

    updated: add dropdown and filter

--- src/pages/listML.jsx
+++ src/pages/listML.jsx
@@ -1,9 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "../assets/loading.svg";
+import { IoFilterOutline } from "react-icons/io5";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+// import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
 
 const ListML = () => {
     const [datas, setDatas] = useState([])
+    const [isOpen, setIsOpen] = useState(false);
+    const [allRole, setAllRole] = useState([])
+    const [filteredRole, setFilteredRole] = useState('')
+    const [activeRole, setActiveRole] = useState("");
+    const [activeSpecialty, setActiveSpecialty] = useState("");
+    const [allSpecialty, setAllSpecialty] = useState([])
+    const [filteredSpecialty, setFilteredSpecialty] = useState('')
     const [search, setSearch] = useState("")
     const [loading, setLoading] = useState(false)
 
@@ -13,13 +29,43 @@ const ListML = () => {
             try {
                 const response = await axios.get("https://api.dazelpro.com/mobile-legends/hero")
                 const data = response.data.hero
+                const uniqueRoles = new Set();
+                const uniqueSpeciallys = new Set();
 
-                if (search.length > 0) {
-                    const filtered = data.filter((item) => item.hero_name.toLowerCase().includes(search.toLowerCase()))
-                    setDatas(filtered)
-                } else {
-                    setDatas(data)
+                data.forEach(hero => {
+                    const roles = hero.hero_role.split(',');
+
+                    roles.forEach(role => {
+                        uniqueRoles.add(role.trim());
+                    });
+                });
+
+                setAllRole([...uniqueRoles])
+
+                data.forEach(hero => {
+                    const speciallys = hero.hero_specially.split(',');
+
+                    speciallys.forEach(specially => {
+                        uniqueSpeciallys.add(specially.trim());
+                    });
+                });
+
+                setAllSpecialty([...uniqueSpeciallys])
+
+                let filteredData = [...data];
+                if (filteredRole) {
+                    filteredData = filteredData.filter(item => item.hero_role.includes(filteredRole));
                 }
+
+                if (filteredSpecialty) {
+                    filteredData = filteredData.filter(item => item.hero_specially.includes(filteredSpecialty));
+                }
+
+                if (search) {
+                    filteredData = filteredData.filter(item => item.hero_name.toLowerCase().includes(search.toLowerCase()));
+                }
+
+                setDatas(filteredData);
             } catch (error) {
                 console.error("Error fetching data. Please try again", error)
             } finally {
@@ -30,33 +76,123 @@ const ListML = () => {
         }
 
         fetchData()
-    }, [search])
+    }, [search, filteredRole, filteredSpecialty])
 
-    console.log("datas : ", datas)
     console.log("search : ", search)
+    console.log("allSpecialty : ", allSpecialty)
+    console.log("filtered role : ", filteredRole)
+    console.log("filtered specialty : ", filteredSpecialty)
+
+    const handleDefault = () => {
+        setFilteredRole("");
+        setFilteredSpecialty("");
+        setActiveRole("");
+        setActiveSpecialty("");
+    }
+
+    const handleRole = (data) => {
+        setFilteredRole(data);
+        setActiveRole(data);
+    };
+
+    const handleSpecialty = (data) => {
+        setFilteredSpecialty(data);
+        setActiveSpecialty(data);
+    };
 
     return (
         <div className="h-[95vh] flex flex-col m-auto gap-5 p-10 mt-10">
             <div className="h-[10vh] flex flex-col gap-5">
                 <h1 className="font-bold text-center uppercase text-2xl">List Hero Mobile Legends</h1>
 
-                <div className="w-[80vw] md:w-[60vw]">
+                <div className="w-[80vw] md:w-[60vw] flex justify-between items-center">
                     <label htmlFor="search" className="flex flex-col">
-                        Search by hero name
                         <input
                             type="text"
                             name="search"
-                            placeholder="search"
-                            className="bg-slate-200 rounded-xl px-5 py-1"
+                            placeholder="search by hero name"
+                            className="bg-slate-200 w-[50vw] rounded-xl px-5 py-1"
                             onChange={(e) => setSearch(e.target.value)}
                         />
                     </label>
+
+                    <div className="relative inline-block text-left">
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            type="button"
+                            className="inline-flex justify-center w-full rounded-full border border-gray-300 shadow-sm p-4 bg-white text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+                            id="options-menu"
+                            aria-haspopup="true"
+                            aria-expanded="true"
+                        >
+                            <IoFilterOutline />
+                        </button>
+
+                        {isOpen && (
+                            <div
+                                className="origin-top-right absolute right-0 mt-2 w-56 h-[65vh] overflow-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                                role="menu"
+                                aria-orientation="vertical"
+                                aria-labelledby="options-menu"
+                            >
+                                <button className="w-[100%] py-2 bg-blue-500 text-white font-bold" onClick={handleDefault}>
+                                    Default
+                                </button>
+                                <div className="py-1" role="none">
+                                    <p className="font-semibold text-sm py-2 px-4 text-center">Roles</p>
+                                    <button
+                                        className={`w-[100%] block px-4 py-2 text-sm text-left hover:bg-gray-100 hover:text-gray-900 ${activeRole === "" ? "bg-blue-200" : ""}`}
+                                        role="menuitem"
+                                        onClick={() => handleRole("")}
+                                    >
+                                        All
+                                    </button>
+                                    {allRole.map((data, index) => {
+                                        return (
+                                            <button
+                                                key={index}
+                                                className={`w-[100%] block px-4 py-2 text-sm text-left hover:bg-gray-100 hover:text-gray-900 ${activeRole === data ? "bg-blue-200" : ""}`}
+                                                role="menuitem"
+                                                onClick={() => handleRole(data)}
+                                            >
+                                                {data}
+                                            </button>
+                                        )
+                                    })}
+
+                                    <br />
+                                    <hr />
+
+                                    <p className="font-semibold text-sm pt-3 pb-2 px-4 text-center">Specialty</p>
+                                    <button
+                                        className={`w-[100%] block px-4 py-2 text-sm text-left hover:bg-gray-100 hover:text-gray-900 ${activeSpecialty === "" ? "bg-blue-200" : ""}`}
+                                        role="menuitem"
+                                        onClick={() => handleSpecialty("")}
+                                    >
+                                        All
+                                    </button>
+                                    {allSpecialty.map((data, index) => {
+                                        return (
+                                            <button
+                                                key={index}
+                                                className={`w-[100%] block px-4 py-2 text-sm text-left hover:bg-gray-100 hover:text-gray-900 ${activeSpecialty === data ? "bg-blue-200" : ""}`}
+                                                role="menuitem"
+                                                onClick={() => handleSpecialty(data)}
+                                            >
+                                                {data}
+                                            </button>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
 
             {loading ? (
                 <div className="w-[60vw] h-[80vh] flex flex-col justify-center items-center">
-                    <img src={Loading} className="w-[50%]" />
+                    <img src={Loading} className="w-[25%]" />
                     <p>Loading</p>
                 </div>
             ) : datas.length === 0 ? (
